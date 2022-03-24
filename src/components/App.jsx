2384b4462918df0e9d679c51f6eb00a583d0e5d7
TheWisePigeon <zozozozeph@gmail.com>
2022-03-24 17:59:39 +0000

    random commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,11 @@
 import react from "react"
-import { Link, Typography, withStyles } from '@mui/material'
+import Header from "./Header"
 
 function App() {
 
   return (
     <div >
-      <header className=" header" >
-        <div>
-          <Typography className=" test" variant="h4"  >
-            Pigeon Head Airdrop
-          </Typography>
-        </div>
-
-        <div className=" action-div">
-          <Link underline="none" href="#" >
-            <Typography variant="h6" color="white" >
-              Login
-            </Typography>
-          </Link><br />
-          <Link underline="none" href="#"  >
-            <Typography variant="h6" color="white">
-              Register
-            </Typography>
-          </Link>
-        </div>
-      </header>
+      <Header></Header>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography, Link } from '@mui/material'
+
+function Header() {
+    return (
+        <div className=" w-full container mx-auto">
+
+            
+        </div>
+    )
+}
+
+export default Header 
